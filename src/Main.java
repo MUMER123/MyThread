bccceada89c933934c0655a80dbeb45ca35bde0e
Muhammad Umer <97335781+MUMER123@users.noreply.github.com>
2023-08-10 17:59:09 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,11 @@ public class Main {
 //        MyThread mt2 = new MyThread("Odd",false);
 //        mt1.start();
 //        mt2.start();
-        MyThread2 mt1 = new MyThread2(false);
-        MyThread2 mt2 = new MyThread2(true);
-        mt1.run();
-        mt2.run();
+       MyThread2 mt2 = new MyThread2(true);
+        MyThread2 mt3 = new MyThread2(false);
+        Thread t1 = new Thread(mt2);
+        Thread t2 = new Thread(mt3);
+        t1.start();
+        t2.start();
     }
 }
